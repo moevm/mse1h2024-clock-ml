729eba05c0119f87ddd87c77b89e36558135a2ec
AlexanderKamynin <kamyninsasa@gmail.com>
2024-02-29 20:47:33 +0300

    init project ml-processing submodule structure

---
+++ mse1h2024-clock-ml-processing/backend-services/restapi/rest.py
@@ -0,0 +1,11 @@
+# import requirements
+import flask
+
+
+class RestAPIService:
+    def __init__(self) -> None:
+        self.__app = flask.Flask("app")
+
+
+if __name__ == "__main__":
+    pass
---
+++ mse1h2024-clock-ml-processing/main.py
@@ -0,0 +1,22 @@
+# import project modules
+# here will be import Estimator class in the future
+
+
+class Core:
+    """Class for init ML-processing modules and REST-API/message broker services.
+
+    This class serves as the core component of the system, responsible for initializing and coordinating
+    machine learning processing modules, REST-API and message broker services.
+
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def start():
+        pass
+
+
+if __name__ == "__main__":
+    core = Core()
+    core.start()
