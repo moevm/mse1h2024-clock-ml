fe4b50dad3f26ca0d8091526f60f262b991704c3
AlexanderKamynin <kamyninsasa@gmail.com>
2024-03-19 17:19:16 +0300

    init: init extractors
    Co-authored-by: Шаврин Алексей Павлович <ShavrinAlex@users.noreply.github.com>

---
+++ mse1h2024-clock-ml-processing/processing/ClockCircleExtractor.py
@@ -0,0 +1,20 @@
+# import requirements
+import cv2
+import numpy as np
+
+
+class ClockCircleExtractor:
+    """_summary_"""
+
+    def __init__(self, image: np.array) -> None:
+        self.__src_image = image
+
+    def get_clock_circle_position(self) -> list:
+        pass
+
+    def __show_clock_circle(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ mse1h2024-clock-ml-processing/processing/ClockHandsExtractor.py
@@ -0,0 +1,20 @@
+# import requirements
+import cv2
+import numpy as np
+
+
+class ClockHandsExtractor:
+    """Class for extracting the positions of clock hands from an image."""
+
+    def __init__(self, image: any) -> None:
+        self.__image = image
+
+    def get_clock_hands_position(self) -> list:
+        pass
+
+    def __show_clock_hands(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    pass
