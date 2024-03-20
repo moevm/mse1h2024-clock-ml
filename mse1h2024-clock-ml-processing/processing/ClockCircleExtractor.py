0ee1da4ecdf5ab23bbfe0dff524da74fb8c7ecaf
SharinAlex <alex.shavrin200303@gmail.com>
2024-03-20 17:36:35 +0300

    refactoring
    Co-authored-by: Alexander Kamynin <AlexanderKamynin@users.noreply.github.com>

--- mse1h2024-clock-ml-processing/processing/ClockCircleExtractor.py
+++ mse1h2024-clock-ml-processing/processing/ClockCircleExtractor.py
@@ -4,18 +4,36 @@ import numpy as np
 
 
 class ClockCircleExtractor:
-    """_summary_"""
+    """Class for extracting the positions and radius for clock circle from the input image"""
 
     def __init__(self, image: np.array) -> None:
+        """
+        Initialization the ClockCircleExtractor.
+
+        Parameters
+        ----------
+        image : np.array
+            Input image with clock
+        
+        """
+        
         self.__src_image = image
         self.__gray_image = cv2.cvtColor(self.__src_image, cv2.COLOR_BGR2GRAY)
         
         self.__clock_circle = None
 
-    def get_clock_circle_position(self) -> list[int, int, int]:
+    def get_extracted(self) -> list[int, int, int] | None:
+        """Return the exctracted clock circle from image or None if not found"""
+        
         return self.__clock_circle
 
     def extract(self) -> None:
+        """
+        Extract clock circle from the input image and save it in class instance. 
+        To get extracted circle use `get_extracted()` method.
+        
+        """
+        
         finded_circles = cv2.HoughCircles(
             image=self.__gray_image,
             method=cv2.HOUGH_GRADIENT_ALT,
@@ -24,45 +42,71 @@ class ClockCircleExtractor:
             param2=0,
             minRadius=0,
             maxRadius=self.__src_image.shape[0],
-        )[0]
-
+        )
+        
+        if finded_circles is None:
+            print('Not found circle on input image')
+            return
+        exctracted = finded_circles[0]
+        
         # define clock circle as circle with maximum radius
-        finded_circles = np.int32(finded_circles)
-        largest_circle = max(finded_circles, key=lambda elem: elem[2])
+        exctracted = np.int32(exctracted)
+        largest_circle = max(exctracted, key=lambda elem: elem[2])
         self.__clock_circle = largest_circle
 
-        self.__show_clock_circle([largest_circle])
+    @staticmethod
+    def show_circles(image: np.array, finded_circles: list[list[int, int, int]], show: bool = True) -> np.array:
+        """
+        Draw the extracted circles on given image.
+        
+        Parameters
+        ----------
+        image : np.array
+            The image on which the extracted circles will be drawn
+        finded_circles : list[list[int, int, int]]
+            Extracted circles that should be drawn. The circle is represented by a list in the following format: [centerX, centerY, radius]
+        show : bool
+            If True, displays the resulting image. Default value is True.
 
-    def __show_clock_circle(self, finded_circles: list) -> None:
+        Returns
+        -------
+        np.array
+            The result image in which the extracted circles are drawn
+        
+        """
+        result_image = image.copy()
         for circle in finded_circles:
             center, radius = (circle[0], circle[1]), circle[2]
             cv2.circle(
-                self.__src_image,
+                result_image,
                 center=center,
                 radius=radius,
                 color=(0, 255, 0),
                 thickness=2,
-            )
+            ) 
             # draw center
             cv2.circle(
-                self.__src_image,
+                result_image,
                 center,
                 radius=10,
                 color=(0, 0, 255),
                 thickness=cv2.FILLED,
             )
 
-        cv2.imshow("circle", self.__src_image)
-        cv2.imwrite("result/circle.png", self.__src_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if show:
+            cv2.imshow("circle", result_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            
+        return result_image
 
 
 if __name__ == "__main__":
     image = cv2.imread("./images/t1.png")
-    extractor = ClockCircleExtractor(
+    circle_extractor = ClockCircleExtractor(
         image=image,
     )
 
-    extractor.extract()
-    print(extractor.get_clock_circle_position())
+    circle_extractor.extract()
+    clock_circle = circle_extractor.get_extracted()
+    # ClockCircleExtractor.show_circles(image, [clock_circle])
--- mse1h2024-clock-ml-processing/processing/ClockHandsExtractor.py
+++ mse1h2024-clock-ml-processing/processing/ClockHandsExtractor.py
@@ -6,111 +6,153 @@ import numpy as np
 class ClockHandsExtractor:
     """Class for extracting the positions of clock hands from an image."""
 
-    def __init__(self, image: np.array, center: list[int] = [444, 437], radius: int = 280) -> None:
+    def __init__(self, image: np.array, center: list[int], radius: int) -> None:
+        """
+        Initialization the ClockHandsExtractor.
+
+        Parameters
+        ----------
+        image : np.array
+            Image object from which numbers are read
+        center : list[int]
+            Сenter point of the dial or image [x, y]
+        radius : int
+            Radius of the dial or the maximum circle inscribed in the image
+        """
+
+        # Initializing an Image Object
         self.__image = image
         self.__gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+        # Initializing a clock hands list
         self.__clock_hands = list()
+        
+        # Initializing a basic constants
         self.__center = center
         self.__center_eps = radius/4
-        self.__min_arrow_length = radius/5
-        self.__tilt_angle_eps = 0.05
+        self.__min_clock_hand_length = radius/5
+        self.__tilt_coef_eps = 0.05
 
-    def get_clock_hands_position(self) -> list:
-        return self.__clock_hands
+    def get_extracted(self) -> list[tuple] | None:
+        """This method returns a list of recognized clock hands"""
+
+        return self.__clock_hands if self.__clock_hands else None
 
     def extract(self) -> None:
+        """This method extracts clock hands from an image"""
+
+        # Initializing the line detector
         line_segment_detector = cv2.createLineSegmentDetector(
             refine=cv2.LSD_REFINE_STD,
-            # sigma_scale=2, #длиннее палки
+            # sigma_scale=2, # длиннее палки
             # quant=2.0,
-            # ang_th = 80, #ideal
-            density_th=0.1,  # ideal
+            # ang_th = 80, # ideal
+            density_th=0.1, # Minimal density of aligned region points in the enclosing rectangle. 
         )
 
+        # Detection of all lines
         lines = line_segment_detector.detect(
             image=self.__gray_image,
         )[0]
 
-        self.__select_arrows(lines)
+        if lines is None:
+            print('Not found lines on input image')
+            return
 
-        # mask = self.__show_clock_hands(lines)
+        # Selecting lines that are clock hands
+        self.__select_clock_hands(lines)
 
-    def __select_arrows(self, lines: list) -> None:
+    def __select_clock_hands(self, lines: list[list[tuple[int, int, int, int]]]) -> None:
+        """"
+        This method selects the lines that are the hands of the clock
         
-        self.__show_clock_hands(lines)
-        tilt_angles = set()
+        Parameters
+        ----------
+        lines : list[list[tuple[int, int, int, int]]]
+            list of lines, among which you need to select the ones responsible for the clock lines.
+            The lines is represented by a list in the following format: [(x1, y1, x2, y2)].
+        """
+
+        # self.show_lines(self.__image, lines)
+        # Initializing set tilt coefficients for recognized clock hands
+        tilt_coefs = set()
+        
+        falls_within = lambda coord, limit, eps: limit - eps <= coord <= limit + eps 
         for line in lines:
             (x1, y1, x2, y2) = line[0]
-            tilt_angle = (y2-y1)/(x2-x1)
+            tilt_coef = (y2-y1)/(x2-x1)
             if (
                 (
-                    self.__center[0] - self.__center_eps <= x1 <= self.__center[0] + self.__center_eps
-                    and self.__center[1] - self.__center_eps <= y1 <= self.__center[1] + self.__center_eps
+                    falls_within(x1, self.__center[0], self.__center_eps)
+                    and falls_within(y1, self.__center[1], self.__center_eps)
                 )
                 or (
-                    self.__center[0] - self.__center_eps <= x2 <= self.__center[0] +  self.__center_eps
-                    and self.__center[1] - self.__center_eps <= y2 <= self.__center[1] +  self.__center_eps
+                    falls_within(x2, self.__center[0], self.__center_eps)
+                    and falls_within(y2, self.__center[1], self.__center_eps)
                 )
-            ) and np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) >= self.__min_arrow_length and not self.__is_exist_tilt_angle(tilt_angle, tilt_angles):
-                # print(line)
-                # add new line
-                self.__clock_hands.append((x1, y1, x2, y2))
-                self.__show_clock_hands([line])
+            ) and np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) >= self.__min_clock_hand_length and \
+                not self.__is_exist_tilt_coef(tilt_coef, tilt_coefs):
                 
-                # update existing tilt angles
-                tilt_angles.add(tilt_angle)
-                print(tilt_angle)
+                # Add new line to clock hands
+                self.__clock_hands.append((x1, y1, x2, y2))
+                # self.show_lines(self.__image, [line])
+                
+                # Update existing tilt coefficients
+                tilt_coefs.add(tilt_coef)
 
-            
-        #for i in range(len(self.__clock_hands)):
+    def __is_exist_tilt_coef(self, tilt_coef: float, tilt_coefs: set[float]) -> bool:
+        """This method checks for the presence of a clock hand with the same slope factor"""
 
-    def __is_exist_tilt_angle(self, tilt_angle: float, tilt_angles: set[float]) -> bool:
-        for cur_tilt_angle in tilt_angles:
-            if cur_tilt_angle - self.__tilt_angle_eps <= tilt_angle <= cur_tilt_angle + self.__tilt_angle_eps:
+        for cur_tilt_coef in tilt_coefs:
+            if cur_tilt_coef - self.__tilt_coef_eps <= tilt_coef <= cur_tilt_coef + self.__tilt_coef_eps:
                 return True
         return False
 
+    @staticmethod    
+    def show_lines(image: np.array, lines: list[list[tuple[int, int, int]]], show: bool = True) -> np.array:
+        """
+        Draw the extracted lines on given image.
         
-    def __show_clock_hands(self, lines: list) -> None:
-        image_with_clock_hands = self.__image.copy()
-        # mask = np.zeros_like(self.__gray_image)
+        Parameters
+        ----------
+        image : np.array
+            The image on which the extracted lines will be drawn
+        lines : list[list[tuple[int, int, int, int]]]]
+            Extracted lines that should be drawn. The lines is represented by a list in the following format: [(x1, y1, x2, y2)]
+        
+        Returns
+        -------
+        np.array
+            The result image in which the extracted lines are drawn
+        
+        """
+
+        image_with_lines = image.copy()
         # Iterate over points
         for points in lines:
             # Extracted points nested in the list
             x1, y1, x2, y2 = points[0]
-            # Draw the lines join the points
-            # On the original image
+
+            # Drawing the lines
             cv2.line(
-                image_with_clock_hands,
+                image_with_lines,
                 tuple(map(int, (x1, y1))),
                 tuple(map(int, (x2, y2))),
                 (0, 255, 0),
                 2,
             )
 
-            # cv2.line(
-            #     mask,
-            #     tuple(map(int, (x1, y1))),
-            #     tuple(map(int, (x2, y2))),
-            #     255,
-            #     2,
-            # )
+        # Displaying an image with lines
+        if show:
+            cv2.imshow("lines", image_with_lines)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-        # Displaying and save an image with clock hands
-        cv2.imshow("clock-hands", image_with_clock_hands)
-        cv2.imwrite("result/clock_hands.png", image_with_clock_hands)
-
-        # cv2.imshow("mask", mask)
-        # cv2.imwrite("result/mask.png", mask)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-        # return mask
+        return image_with_lines
 
 
 if __name__ == "__main__":
-    che = ClockHandsExtractor(cv2.imread("./images/t1.png"), [450, 421], 399)
+    che = ClockHandsExtractor(cv2.imread("./images/t1.png"), [400, 400], 399)
     che.extract()
-    lines = che.get_clock_hands_position()
-    print(lines, len(lines))
+    lines = che.get_extracted()
+    print(lines)
