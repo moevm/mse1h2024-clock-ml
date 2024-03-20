20e83ea5be817377a78f1158eb8666106271f362
SharinAlex <alex.shavrin200303@gmail.com>
2024-03-20 19:50:26 +0300

    update: check extractors work
    Co-authored-by: Alexander Kamynin <AlexanderKamynin@users.noreply.github.com>

--- mse1h2024-clock-ml-processing/processing/Estimator.py
+++ mse1h2024-clock-ml-processing/processing/Estimator.py
@@ -1,20 +1,41 @@
 # import requirements
+import cv2
 import numpy as np
 
 # import project modules
 from ClockCircleExtractor import ClockCircleExtractor
 from ClockHandsExtractor import ClockHandsExtractor
-from DigitsPositionExtractor import DigitsPositionExtractor
+from ClockDigitsExtractor import ClockDigitsExtractor
 
 
 class Estimator:
     """__summary__"""
 
     def __init__(self) -> None:
-        self.__clock_circle_extrator = None
-        self.__clock_hands_extractor = None
-        self.__digits_position_extractor = None
+        self.__clock_circle_extrator = ClockCircleExtractor()
+        self.__clock_hands_extractor = ClockHandsExtractor()
+        self.__clock_digits_extractor = ClockDigitsExtractor()
         
-    def estimate(self, image: np.array) -> int:
+    def estimate(self, image: np.array, time: int = 0) -> int:
+        estimation_result = 0
         
-        return 10
+        digits = self.__clock_digits_extractor.extract(image)
+        circle = self.__clock_circle_extrator.extract(image) 
+        hands = self.__clock_hands_extractor.extract(image, circle[0:2], circle[2])
+        
+        print(f"Digits = {digits}")
+        print(f"Circle = {circle}")
+        print(f"Hands = {hands}")
+
+        if digits is None:
+            estimation_result = 1
+        
+        return estimation_result
+    
+    
+if __name__ == '__main__':
+    estimator = Estimator()
+    image = cv2.imread("./images/circle.png")
+    
+    estimation_result = estimator.estimate(image)
+    print(f"Esimation result is equal {estimation_result}")
