ab8361aabc117cf6e2f7e2e697206add0b9b8ce2
ormequ <ilya.belokobylsky@gmail.com>
2024-04-09 22:07:56 +0300

    fix: structure

--- mse1h2024-clock-ml-backend/internal/httpserver/dto.go
+++ mse1h2024-clock-ml-backend/internal/httpserver/dto.go
@@ -3,13 +3,13 @@ package httpserver
 // ImageRequest is a request to upload an image into ml-service.
 type ImageRequest struct {
 	// Image is the binary image.
-	Image []byte
+	Image []byte `json:"image" binding:"required"`
 	// IsBroker is flag that broker should be used.
-	IsBroker bool
+	IsBroker bool `json:"is_broker" binding:"required"`
 	// Hours is the number of hours in the image to check.
-	Hours int
+	Hours int `json:"hours" binding:"required"`
 	// Hours is the number of minutes in the image to check.
-	Minutes int
+	Minutes int `json:"minutes" binding:"required"`
 }
 
 type ErrorResponse struct {
