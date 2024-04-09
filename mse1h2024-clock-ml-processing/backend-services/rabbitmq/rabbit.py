bd6564cac2cfeaca82bf5b09af6479144970ef55
tapehavladimir@gmail.com <tapehavladimir@gmail.com>
2024-04-09 21:44:42 +0300

    feat: add rmq service

--- mse1h2024-clock-ml-processing/backend-services/rabbitmq/rabbit.py
+++ mse1h2024-clock-ml-processing/backend-services/rabbitmq/rabbit.py
@@ -1,8 +1,56 @@
-# import requirements
+import asyncio
+from functools import partial
+import aio_pika
+import importlib
+from PIL import Image
+import io
+import json
 
+estimationModule = importlib.import_module("processing.Estimator")
 
-class RabbitMQService:
-    """_summary_"""
+class RabbitmMQService:
+    def __init__(self, estimator: any):
+        self.__estimator = estimator
 
-    def __init__(self) -> None:
-        pass
+    async def consumer(
+        self, msg: aio_pika.IncomingMessage,
+        channel: aio_pika.RobustChannel
+    ):
+        async with msg.process():
+            image_data = msg.body
+            image = Image.open(io.BytesIO(image_data))
+            
+            result = self.__estimator.estimate(image)
+            
+            response = json.dumps({"result": result})
+            
+            if msg.reply_to:
+                await channel.default_exchange.publish(
+                    message=aio_pika.Message(
+                        body=response.encode(),
+                        correlation_id=msg.correlation_id,
+                    ),
+                    routing_key=msg.reply_to,
+                )
+
+async def main():
+    connection = await aio_pika.connect_robust(
+        "amqp://guest:guest@127.0.0.1/"
+    )
+
+    queue_name = "estimation"
+
+    async with connection:
+        channel = await connection.channel()
+        queue = await channel.declare_queue(queue_name)
+        
+        service = RabbitmMQService(estimationModule)  
+        await queue.consume(partial(service.consumer, channel=channel))  
+
+        try:
+            await asyncio.Future()
+        except Exception:
+            pass
+
+if __name__ == '__main__':
+    asyncio.run(main())
