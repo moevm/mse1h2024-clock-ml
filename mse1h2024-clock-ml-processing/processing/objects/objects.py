431fdf8f2a5df85296979bb219f4cd5b3ff2d29c
AlexanderKamynin <kamyninsasa@gmail.com>
2024-04-11 15:57:00 +0300

    update: clock circle obj implement

--- mse1h2024-clock-ml-processing/processing/objects/objects.py
+++ mse1h2024-clock-ml-processing/processing/objects/objects.py
@@ -1,6 +1,22 @@
+# import requirements
+import numpy as np
+from typing import Union
+
+
 class ClockCircle:
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self, center_coordinates: Union[tuple, list, np.ndarray], radius: int
+    ) -> None:
+        self._center_coordinates = (center_coordinates[0], center_coordinates[1])
+        self._radius = radius
+
+    @property
+    def center_coordinates(self) -> tuple[int, int]:
+        return self._center_coordinates
+
+    @property
+    def radius(self) -> int:
+        return self._radius
 
 
 class ClockHands:
@@ -11,3 +27,8 @@ class ClockHands:
 class ClockDigits:
     def __init__(self) -> None:
         pass
+
+
+if __name__ == "__main__":
+    circle = ClockCircle(center_coordinates=[1, 2], radius=5)
+    print(circle.center_coordinates, circle.radius)
