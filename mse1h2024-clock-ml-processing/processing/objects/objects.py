5a9e84469ebe046be0c002dc9f524e2f7c29ed8f
AlexanderKamynin <kamyninsasa@gmail.com>
2024-04-11 15:41:37 +0300

    initial commit Co-authored-by: Шаврин Алексей Павлович <ShavrinAlex@users.noreply.github.com>

---
+++ mse1h2024-clock-ml-processing/processing/objects/objects.py
@@ -0,0 +1,13 @@
+class ClockCircle:
+    def __init__(self) -> None:
+        pass
+
+
+class ClockHands:
+    def __init__(self) -> None:
+        pass
+
+
+class ClockDigits:
+    def __init__(self) -> None:
+        pass
