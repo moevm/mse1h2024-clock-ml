163df03f0fade9099059c18839f4091d2a3f872f
AlexanderKamynin <kamyninsasa@gmail.com>
2024-04-16 15:56:20 +0300

    update: extractor tests implement
    Co-authored-by: Шаврин Алексей Павлович <ShavrinAlex@users.noreply.github.com>

--- mse1h2024-clock-ml-processing/tests/extractor_test.py
+++ mse1h2024-clock-ml-processing/tests/extractor_test.py
@@ -1,9 +1,73 @@
-from processing.extractors.ClockCircleExtractor import ClockCircleExtractor
+import cv2
 
 
-class TestExtractors:
-    def test_circle_extractor(self):
-        pass
+class TestCircleExtractor:
+    from processing.extractors.ClockCircleExtractor import ClockCircleExtractor
 
-    def test_digits_extractor(self):
-        pass
+    extractor = ClockCircleExtractor()
+
+    def test_extract_not_none(self):
+        try:
+            image = cv2.imread("tests/images/10.png")
+            result = self.extractor.extract(image=image)
+        except Exception as e:
+            print(f"Some problem in test clock extractor: {e}")
+
+        assert result is not None
+
+    def test_extract_none(self):
+        try:
+            image = cv2.imread("tests/images/1.png")
+            result = self.extractor.extract(image=image)
+        except Exception as e:
+            print(f"Some problem in test clock extractor: {e}")
+
+        assert result is None
+
+
+class TestDigitsExtractor:
+    from processing.extractors.ClockDigitsExtractor import ClockDigitsExtractor
+
+    extractor = ClockDigitsExtractor()
+
+    def test_extract_not_none(self):
+        try:
+            image = cv2.imread("tests/images/10.png")
+            result = self.extractor.extract(image=image)
+        except Exception as e:
+            print(f"Some problem in test digits extractor: {e}")
+
+        assert result is not None
+
+    def test_extract_none(self):
+        try:
+            image = cv2.imread("tests/images/1.png")
+            result = self.extractor.extract(image=image)
+        except Exception as e:
+            print(f"Some problem in test digits extractor: {e}")
+
+        assert result is None
+
+
+class TestClockHandsExtractor:
+    from processing.extractors.ClockHandsExtractor import ClockHandsExtractor
+
+    extractor = ClockHandsExtractor()
+
+    def test_extract_not_none(self):
+        try:
+            image = cv2.imread("tests/images/10.png")
+            result = self.extractor.extract(image=image, center=[469, 221], radius=192)
+        except Exception as e:
+            print(f"Some problem in test digits extractor: {e}")
+
+        assert result is not None
+
+    def test_extract_none(self):
+        try:
+            image = cv2.imread("tests/images/1.png")
+            result = self.extractor.extract(image=image, center=[0, 0], radius=1)
+        except Exception as e:
+            print(f"Some problem in test digits extractor: {e}")
+
+        assert result is None
