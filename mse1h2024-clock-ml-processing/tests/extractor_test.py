a0316337911ab07a8ab0c2a7bae84db67f10e32b
AlexanderKamynin <kamyninsasa@gmail.com>
2024-04-16 14:23:24 +0300

    initial commit with __init__ files
    Co-authored-by: Шаврин Алексей Павлович <ShavrinAlex@users.noreply.github.com>

---
+++ mse1h2024-clock-ml-processing/tests/extractor_test.py
@@ -0,0 +1,9 @@
+from processing.extractors.ClockCircleExtractor import ClockCircleExtractor
+
+
+class TestExtractors:
+    def test_circle_extractor(self):
+        pass
+
+    def test_digits_extractor(self):
+        pass
