ab33f761c4992fd72308665d80a65722e7e37d2f
SharinAlex <alex.shavrin200303@gmail.com>
2024-05-21 14:46:52 +0300

    update: add documentation to init method in ClockHands object

--- mse1h2024-clock-ml-processing/processing/objects/objects.py
+++ mse1h2024-clock-ml-processing/processing/objects/objects.py
@@ -47,8 +47,9 @@ class ClockCircle:
 
 class ClockHands:
     """Class for keeping the information about clock hands"""
-    
+
     def __init__(self, lines: list[tuple[int, int, int, int]]) -> None:
+        """Initialization the ClockHands"""
         self._clock_hands = lines.copy()
         self._angles = {"hour": 0, "minute": 0}
 
