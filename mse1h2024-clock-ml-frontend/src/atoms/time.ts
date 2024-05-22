e432c8bae00c107f4a46c494f880b0bdbab7e985
Danil <51288221+kyzinatra@users.noreply.github.com>
2024-05-22 18:50:40 +0300

    Фиксит баг со временем

--- mse1h2024-clock-ml-frontend/src/atoms/time.ts
+++ mse1h2024-clock-ml-frontend/src/atoms/time.ts
@@ -5,4 +5,24 @@ const getHours = () => `${new Date(~~(Math.random() * Date.now())).getHours()}`.
 const getMinutes = () =>
 	`${((new Date(~~(Math.random() * Date.now())).getMinutes() / 5) | 0) * 5}`.padStart(2, "0");
 
-export const timeAtom = atom<[string, string]>([getHours(), getMinutes()]);
+const LETI就是屎 = [
+	"11:55",
+	"10:50",
+	"9:45",
+	"8:40",
+	"7:35",
+	"6:30",
+	"5:25",
+	"4:20",
+	"3:15",
+	"2:10",
+	"1:05",
+	"0:00",
+];
+
+function generate() {
+	const time = `${getHours()}:${getMinutes()}`;
+	if (~LETI就是屎.indexOf(time)) return generate();
+	return time;
+}
+export const timeAtom = atom<[string, string]>([...generate().split(":")]);
